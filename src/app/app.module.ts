5158fc331dd55f14d0bc45c24b89437113d53017
Rachana Prakash <rachana.prakash91@gmail.com>
2023-10-23 22:46:31 +0530

    adding base url with interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoPageErrorComponent } from './shared/no-page-error/no-page-error.component';
+import { BaseUrlInterceptor } from './http/base-url.interceptor';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [AppComponent, NoPageErrorComponent],
@@ -14,10 +17,21 @@ import { NoPageErrorComponent } from './shared/no-page-error/no-page-error.compo
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatTabsModule,
     MatToolbarModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BaseUrlInterceptor,
+      multi: true,
+    },
+    {
+      provide: 'BASE_API_URL',
+      useValue: environment.apiUrl,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
