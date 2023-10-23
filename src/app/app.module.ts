477a92dd3757016eb57cf075da00c4fe62302853
Rachana Prakash <rachana.prakash91@gmail.com>
2023-10-23 18:06:24 +0530

    base style for page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoPageErrorComponent } from './shared/no-page-error/no-page-error.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { NoPageErrorComponent } from './shared/no-page-error/no-page-error.compo
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTabsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
